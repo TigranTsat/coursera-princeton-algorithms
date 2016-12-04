5a3fd448f5208e27a02ae7f6ea364f3fa326bca9
Tigran Tsaturyan <tigran@Tigrans-MacBook-Air.local>
2016-12-04 13:21:36 -0800

    Some tests for week4

---
+++ part1/week4-part1/src/Week4IQ.java
@@ -0,0 +1,40 @@
+
+public class Week4IQ {
+    public static void main(String[] args) {
+        Week4IQ w = new Week4IQ();
+        w.subtask1();
+    }
+
+    private void subtask1() {
+        double a = 0.0;
+        double b = -0.0;
+        Double x = new Double(a);
+        Double y = new Double(b);
+        if (a == b) {
+            if (!x.equals(y)) {
+                System.out.println("Yay!");
+            } else {
+                System.out.println("Nope 1");
+            }
+        } else {
+            System.out.println("Nope 2");
+        }
+    }
+
+    private void subtask2() {
+        double a = Double.NaN;
+        double b = Double.NaN;
+        Double x = new Double(a);
+        Double y = new Double(b);
+        if (a != b) {
+            if (x.equals(y)) {
+                System.out.println("Yay!");
+            } else {
+                System.out.println("Nope 1");
+            }
+        }
+        else {
+            System.out.println("Nope 2");
+        }
+    }
+}
